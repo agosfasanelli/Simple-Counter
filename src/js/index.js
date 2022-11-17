7d69e04202cec8a04a49ad64e4cb860cbc888e8a
agosfasanelli <agosfasanelli@gmail.com>
2022-11-17 10:06:08 +0000

    jhasgvhxaj

--- src/js/index.js
+++ src/js/index.js
@@ -13,27 +13,18 @@ function Counter(props){
 				<div className="row bg-black mt-3 ms-3 me-3">
 					<div className="col-8 mx-auto d-inline-flex text-center">
 						<div id="reloj"><i className="fa-regular fa-clock"></i></div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitSeven % 10}</div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitSix % 10}</div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitFive % 10}</div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitFour % 10}</div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitThree % 10}</div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitTwo % 10}</div>
-						<div id="recuadros" className="text-white m-3 fw-bold">{props.digitOne % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberSeven % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberSix % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberFive % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberFour % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberThree % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberTwo % 10}</div>
+						<div id="recuadros" className="text-white m-3 fw-bold">{props.numberOne % 10}</div>
 					</div>
 				</div>
 			</div>
 }
 	
-	Counter.propTypes = {
-		digitSeven: propTypes.number,
-		digitSix: propTypes.number,
-		digitFive: propTypes.number,
-		digitFour: propTypes.number,
-		digitThree: propTypes.number,
-		digitTwo: propTypes.number,
-		digitOne: propTypes.number
-	}
 	let Count = 0;
 	setInterval(function(){
 		const seven = Math.floor(Count/ 1000000)
@@ -43,17 +34,17 @@ function Counter(props){
 		const three = Math.floor(Count/ 100)
 		const two = Math.floor(Count/ 10)
 		const one = Math.floor(Count/ 1)
-		console.log(seven, six, five, four, three, two, one)
-		Count ++;	
+		Count = Count + 1 	
+		
         ReactDOM.render(
         <Counter
-            digitOne ={one} 
-            digitTwo ={two} 
-            digitThree ={three} 
-            digitFour ={four} 
-            digitFive ={five} 
-            digitSix ={six} 
-            digitSeven ={seven} 
+            numberOne ={one} 
+            numberTwo ={two} 
+            numberThree ={three} 
+            numberFour ={four} 
+            numberFive ={five} 
+            numberSix ={six} 
+            numberSeven ={seven} 
             />, document.querySelector("#app"));	
 },1000)
 
